446221440306d1799cb746f835f22f4e6efc440b
ngorham <ngorham2@gmail.com>
2018-06-19 02:38:07 -0700

    ProjectTower Update 4 6/19/2018

--- app/src/main/java/net/ngorham/projecttower/PlayerFragment.java
+++ app/src/main/java/net/ngorham/projecttower/PlayerFragment.java
@@ -44,6 +44,7 @@ public class PlayerFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
         }
@@ -51,6 +52,7 @@ public class PlayerFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_player, container, false);
     }
